482f944bd10780c733ca6f2fdfde51e5dfd55b01
praneethcheturi-143 <praneethcheturi@gmail.com>
2026-06-17 20:52:36 -0400

    Fix models/index.js content

--- server/index.js
+++ server/index.js
@@ -4,7 +4,6 @@ const http = require('http');
 const { Server } = require('socket.io');
 require('dotenv').config();
 const { sequelize } = require('./models');
-sequelize.sync({ alter: true }).then(() => console.log('Database synced'));
 
 const app = express();
 const server = http.createServer(app);
@@ -15,8 +14,14 @@ const io = new Server(server, {
 app.use(cors());
 app.use(express.json());
 
+sequelize.sync({ alter: true }).then(() => console.log('Database synced'));
+
 app.get('/', (req, res) => {
   res.json({ message: 'CollabFlow API running' });
+});
+
+app.get('/api/health', (req, res) => {
+  res.json({ status: 'ok', timestamp: new Date() });
 });
 
 const authRoutes = require('./routes/auth');
@@ -27,19 +32,45 @@ app.use('/api/auth', authRoutes);
 app.use('/api/boards', boardRoutes);
 app.use('/api/cards', cardRoutes);
 
+const boardUsers = {};
+
 io.on('connection', (socket) => {
-  socket.on('join-board', (boardId) => {
+  socket.on('join-board', ({ boardId, username }) => {
     socket.join(boardId);
+    socket.data.boardId = boardId;
+    socket.data.username = username;
+    if (!boardUsers[boardId]) boardUsers[boardId] = [];
+    if (!boardUsers[boardId].includes(username)) {
+      boardUsers[boardId].push(username);
+    }
+    io.to(boardId).emit('board-users', boardUsers[boardId]);
   });
+
+  socket.on('leave-board', ({ boardId, username }) => {
+    if (boardUsers[boardId]) {
+      boardUsers[boardId] = boardUsers[boardId].filter(u => u !== username);
+      io.to(boardId).emit('board-users', boardUsers[boardId]);
+    }
+  });
+
   socket.on('card-moved', (data) => {
     socket.to(data.boardId).emit('card-moved', data);
   });
+
   socket.on('card-created', (data) => {
     socket.to(data.boardId).emit('card-created', data);
   });
+
+  socket.on('disconnect', () => {
+    const { boardId, username } = socket.data;
+    if (boardId && username && boardUsers[boardId]) {
+      boardUsers[boardId] = boardUsers[boardId].filter(u => u !== username);
+      io.to(boardId).emit('board-users', boardUsers[boardId]);
+    }
+  });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3001;
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
--- server/models/index.js
+++ server/models/index.js
@@ -1,76 +1,16 @@
-const express = require('express');
-const cors = require('cors');
-const http = require('http');
-const { Server } = require('socket.io');
-require('dotenv').config();
-const { sequelize } = require('./models');
+const sequelize = require('../config/database');
+const User = require('./User');
+const Board = require('./Board');
+const Column = require('./Column');
+const Card = require('./Card');
 
-const app = express();
-const server = http.createServer(app);
-const io = new Server(server, {
-  cors: { origin: '*' }
-});
+User.hasMany(Board, { foreignKey: 'ownerId' });
+Board.belongsTo(User, { foreignKey: 'ownerId' });
 
-app.use(cors());
-app.use(express.json());
+Board.hasMany(Column, { foreignKey: 'boardId' });
+Column.belongsTo(Board, { foreignKey: 'boardId' });
 
-sequelize.sync({ alter: true }).then(() => console.log('Database synced'));
+Column.hasMany(Card, { foreignKey: 'columnId' });
+Card.belongsTo(Column, { foreignKey: 'columnId' });
 
-app.get('/', (req, res) => {
-  res.json({ message: 'CollabFlow API running' });
-});
-
-app.get('/api/health', (req, res) => {
-  res.json({ status: 'ok', timestamp: new Date() });
-});
-
-const authRoutes = require('./routes/auth');
-const boardRoutes = require('./routes/boards');
-const cardRoutes = require('./routes/cards');
-
-app.use('/api/auth', authRoutes);
-app.use('/api/boards', boardRoutes);
-app.use('/api/cards', cardRoutes);
-
-const boardUsers = {};
-
-io.on('connection', (socket) => {
-  socket.on('join-board', ({ boardId, username }) => {
-    socket.join(boardId);
-    socket.data.boardId = boardId;
-    socket.data.username = username;
-    if (!boardUsers[boardId]) boardUsers[boardId] = [];
-    if (!boardUsers[boardId].includes(username)) {
-      boardUsers[boardId].push(username);
-    }
-    io.to(boardId).emit('board-users', boardUsers[boardId]);
-  });
-
-  socket.on('leave-board', ({ boardId, username }) => {
-    if (boardUsers[boardId]) {
-      boardUsers[boardId] = boardUsers[boardId].filter(u => u !== username);
-      io.to(boardId).emit('board-users', boardUsers[boardId]);
-    }
-  });
-
-  socket.on('card-moved', (data) => {
-    socket.to(data.boardId).emit('card-moved', data);
-  });
-
-  socket.on('card-created', (data) => {
-    socket.to(data.boardId).emit('card-created', data);
-  });
-
-  socket.on('disconnect', () => {
-    const { boardId, username } = socket.data;
-    if (boardId && username && boardUsers[boardId]) {
-      boardUsers[boardId] = boardUsers[boardId].filter(u => u !== username);
-      io.to(boardId).emit('board-users', boardUsers[boardId]);
-    }
-  });
-});
-
-const PORT = process.env.PORT || 3001;
-server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+module.exports = { sequelize, User, Board, Column, Card };
